25d6b35116b92d84fd1453b34a289d069586776d
Bright Iyahen <bightiyahen@gmail.com>
2023-07-15 23:28:44 +0200

    Just added web_apps function

--- web_apps.js
+++ web_apps.js
@@ -1,5 +1,5 @@
 // trying things out
 function web_apps(){
-    console.log("We are going to build some great apps")
+    console.log("We are going to build some great apps for the world to see")
 }
 web_apps()
